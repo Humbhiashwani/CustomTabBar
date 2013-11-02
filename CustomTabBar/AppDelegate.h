50508424d9e264a7c97ec89e0730f539f5c5bd55
Sergey Demchenko <s_demchenko@kenlo-group.com>
2013-11-02 09:46:22 +0200

    Initial Commit

---
+++ CustomTabBar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomTabBar
+//
+//  Created by Sergey Demchenko on 11/2/13.
+//  Copyright (c) 2013 antrix1989@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomTabBar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomTabBar
+//
+//  Created by Sergey Demchenko on 11/2/13.
+//  Copyright (c) 2013 antrix1989@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
